370cd71aa8537731407136e62eceff62a0061cd1
Bùi Ngọc Lễ <buingocle.vp@gmail.com>
2017-12-04 13:24:57 +0700

    check null statusText

--- eventsource.js
+++ eventsource.js
@@ -486,7 +486,7 @@
       } else if (status !== 0) {
         var message = "";
         if (status !== 200) {
-          message = "EventSourceEx's response has a status " + status + " " + statusText.replace(/\s+/g, " ") + " that is not 200. Aborting the connection.";
+          message = "EventSourceEx's response has a status " + status + " " + (statusText+"").replace(/\s+/g, " ") + " that is not 200. Aborting the connection.";
         } else {
           message = "EventSourceEx's response has a Content-Type specifying an unsupported type: " + contentType.replace(/\s+/g, " ") + ". Aborting the connection.";
         }
